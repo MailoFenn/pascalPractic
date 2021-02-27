21533c03a8b3f972d17302c587252848c1922e93
Mailo_Fenn <nikitakulkov7@gmail.com>
2021-02-27 20:05:51 +0300

    Block2_1 ok

---
+++ Block2/block2_1.pas
@@ -0,0 +1,14 @@
+﻿var x, y: integer;
+
+begin
+  writeln('Введите X');
+  readln(x);
+  writeln('Введите Y');
+  readln(y);
+  if (x < 0) and (y > 0) then begin
+    writeln('Точка лежит во второй четверти.');
+  end
+  else begin
+    writeln('Точка не лежит во второй четверти.');
+  end;
+end.
