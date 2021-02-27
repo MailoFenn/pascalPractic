6cea59079cab7bb609394b8ad0881a1f8aa3fb87
Mailo_Fenn <nikitakulkov7@gmail.com>
2021-02-27 20:25:05 +0300

    all blok2 ok

---
+++ Block2/block2_4.pas
@@ -0,0 +1,12 @@
+﻿var first, second: real;
+
+begin
+  write('Введите первое число: ');
+  readln(first);
+  write('Введите второе число: ');
+  readln(second);
+  if abs(first) > abs(second) then begin
+    first := first / 2;
+   end;
+  writeln('Первое число = ', first, ', второе число = ', second);
+end.
