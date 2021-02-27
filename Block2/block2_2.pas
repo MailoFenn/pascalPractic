5844d4e2376d076c4749ba69e389cdb7bd28764e
Mailo_Fenn <nikitakulkov7@gmail.com>
2021-02-27 20:06:27 +0300

    Block2_2 ok

---
+++ Block2/block2_2.pas
@@ -0,0 +1,22 @@
+﻿var a, b, c: integer;
+
+begin
+  write('Введите A: ');
+  readln(a);
+  write('Введите B: ');
+  readln(b);
+  write('Введите C: ');
+  readln(c);
+  if (a > 0) and (b < 0) and (c < 0) then begin
+    writeln('Проверка пройдена!');
+  end
+  else if (a < 0) and (b > 0) and (c < 0) then begin
+    writeln('Проверка пройдена!');
+  end
+  else if (a < 0) and (b < 0) and (c > 0) then begin
+    writeln('Проверка пройдена!');
+  end
+  else begin
+    writeln('Проверка не пройдена...')
+  end;
+end.
