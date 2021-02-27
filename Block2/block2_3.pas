6ba958cfe21d5758205e3b294d12cf97e2ef3829
Mailo_Fenn <nikitakulkov7@gmail.com>
2021-02-27 20:18:26 +0300

    bloc2_3 ok

---
+++ Block2/block2_3.pas
@@ -0,0 +1,19 @@
+﻿var a, b: integer;
+
+begin
+  write('Введите A: ');
+  readln(a);
+  write('Введите B: ');
+  readln(b);
+  if a > b then begin
+    a := b;
+  end
+  else if a < b then begin
+    b := a;
+  end
+  else begin
+   a := 0;
+   b := 0;
+  end;
+  writeln('A = ', a, ', B = ', b);
+end.
